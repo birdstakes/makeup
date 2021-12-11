779620ab4aa1efd67e31cfcddad9cff06d0d70a1
Josh Steffen <josh0x53@gmail.com>
2021-12-10 19:00:07 -0500

    Use match in parser

--- makeup/parser.py
+++ makeup/parser.py
@@ -53,65 +53,63 @@ def parse(input: str) -> Types:
     enums = {}
 
     def get_type(node) -> Type:
-        if isinstance(node, c_ast.Decl):
-            return get_type(node.type)
+        match node:
+            case c_ast.Decl():
+                return get_type(node.type)
 
-        elif isinstance(node, c_ast.TypeDecl):
-            return get_type(node.type)
+            case c_ast.TypeDecl():
+                return get_type(node.type)
 
-        elif isinstance(node, c_ast.IdentifierType):
-            if node.names[0] in typedefs:
-                return typedefs[node.names[0]]
-            return BuiltinType(node.names)
+            case c_ast.IdentifierType():
+                if node.names[0] in typedefs:
+                    return typedefs[node.names[0]]
+                return BuiltinType(node.names)
 
-        elif isinstance(node, c_ast.PtrDecl):
-            return PointerType(get_type(node.type))
+            case c_ast.PtrDecl():
+                return PointerType(get_type(node.type))
 
-        elif isinstance(node, c_ast.ArrayDecl):
-            if isinstance(node.dim, c_ast.Constant):
+            case c_ast.ArrayDecl(dim=c_ast.Constant()):
                 return ArrayType(get_type(node.type), int(node.dim.value, 0))
-            else:
-                return UnhandledType()
 
-        elif isinstance(node, c_ast.Typedef):
-            type = get_type(node.type)
-            typedefs[node.name] = type
-            return type
+            case c_ast.Typedef():
+                type = get_type(node.type)
+                typedefs[node.name] = type
+                return type
 
-        elif isinstance(node, c_ast.Struct):
-            if node.decls is None:
+            case c_ast.Struct(decls=None):
                 if node.name not in structs:
                     structs[node.name] = StructType([])
                 return structs[node.name]
 
-            fields = []
-            for decl in node.decls:
-                fields.append((decl.name, get_type(decl.type)))
+            case c_ast.Struct():
+                fields = []
+                for decl in node.decls:
+                    fields.append((decl.name, get_type(decl.type)))
 
-            struct = structs.get(node.name, StructType([]))
-            struct.fields = fields
-            if node.name is not None:
-                structs[node.name] = struct
+                struct = structs.get(node.name, StructType([]))
+                struct.fields = fields
+                if node.name is not None:
+                    structs[node.name] = struct
 
-            return struct
+                return struct
 
-        elif isinstance(node, c_ast.Enum):
-            if node.values is None:
+            case c_ast.Enum(values=None):
                 if node.name not in enums:
                     enums[node.name] = EnumType([])
                 return enums[node.name]
 
-            enumerators = [e.name for e in node.values.enumerators]
+            case c_ast.Enum():
+                enumerators = [e.name for e in node.values.enumerators]
 
-            enum = enums.get(node.name, EnumType([]))
-            enum.enumerators = enumerators
-            if node.name is not None:
-                enums[node.name] = enum
+                enum = enums.get(node.name, EnumType([]))
+                enum.enumerators = enumerators
+                if node.name is not None:
+                    enums[node.name] = enum
 
-            return enum
+                return enum
 
-        else:
-            return UnhandledType()
+            case _:
+                return UnhandledType()
 
     for node in ast:
         get_type(node)
