ec47e6263738a9dca57b2d79bb5f38c9cdda0c4b
Josh Steffen <josh0x53@gmail.com>
2021-12-13 18:57:36 -0500

    Expose more options to cli

--- makeup/main.py
+++ makeup/main.py
@@ -13,11 +13,20 @@ app = typer.Typer()
 def main(
     input: typer.FileText = typer.Argument(..., allow_dash=True),
     output: Optional[typer.FileTextWrite] = typer.Argument(None),
+    indent_size: int = typer.Option(2),
+    max_array_size: int = typer.Option(10),
+    max_depth: int = typer.Option(4),
     follow_pointers: bool = typer.Option(True),
 ):
     """
     Generate a pretty-printer for C structs
     """
     input_text = typer.open_file(str(input)).read()
-    output_text = generate(parse(input_text), follow_pointers=follow_pointers)
+    output_text = generate(
+        parse(input_text),
+        indent_size=indent_size,
+        max_array_size=max_array_size,
+        max_depth=max_depth,
+        follow_pointers=follow_pointers,
+    )
     typer.echo(output_text, output)
